27eda99b0613c26012497cc0fabe98952dac49e7
Kseniya Harpushkina <Ksusha-gar98@yandex.ru>
2026-03-27 12:40:39 +0100

    Fix local storage value setting

--- src/utils/unlockedLevels.js
+++ src/utils/unlockedLevels.js
@@ -7,8 +7,12 @@ export const STORAGE_KEYS = {
 export const getHighestUnlockedLevel = () => {
     const value = Number(localStorage.getItem("highestUnlockedLevel"));
 
-    if (!Number.isInteger(value)) {
+    if (!value || !Number.isInteger(value) || value < 1) {
         setHighestUnlockedLevel(1);
+    }
+
+    if (value > levelsCount) {
+        setHighestUnlockedLevel(value);
     }
 
     return Phaser.Math.Clamp(value, 1, levelsCount);
